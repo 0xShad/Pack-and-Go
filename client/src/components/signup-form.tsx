14740cceecb8be490624c7711e4a2ddc314f71a2
shadrack <castroshadrack7@gmail.com>
2025-04-21 11:21:02 +0800

    feat(auth): implement login and signup forms with switch functionality

--- client/src/components/signup-form.tsx
+++ client/src/components/signup-form.tsx
@@ -10,7 +10,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export function LoginForm({
+export function SignUpForm({
   className,
   onSwitch,
   ...props
@@ -19,9 +19,9 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader>
-          <CardTitle>Login to your account</CardTitle>
+          <CardTitle>Create an account</CardTitle>
           <CardDescription>
-            Enter your email below to login to your account
+            Fill the form to create your account.
           </CardDescription>
         </CardHeader>
         <CardContent>
@@ -44,14 +44,14 @@ export function LoginForm({
               </div>
               <div className="flex flex-col gap-3">
                 <Button type="submit" className="w-full">
-                  Login
+                  Sign up
                 </Button>
               </div>
             </div>
             <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
+              Already have an account?{" "}
               <button type="button" onClick={onSwitch} className="underline underline-offset-4 cursor-pointer">
-                Sign up
+                Login
               </button>
             </div>
           </form>
--- client/src/pages/authpage.tsx
+++ client/src/pages/authpage.tsx
@@ -1,10 +1,15 @@
 import { LoginForm } from "@/components/login-form"
+import { SignUpForm } from "@/components/signup-form"
+import { useState } from "react"
 
 export default function AuthPage() {
+  const [isLogin, setIsLogin] = useState(false)
+
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm />
+        {isLogin ? (<LoginForm onSwitch={() => setIsLogin(false)}/>) : <SignUpForm onSwitch={() => setIsLogin(true)}/>}
       </div>
     </div>
   )
