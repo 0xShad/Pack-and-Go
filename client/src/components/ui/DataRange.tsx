ab7856b54f0826de3e726674af128f30e87e5e32
shadrack <castroshadrack7@gmail.com>
2025-04-25 13:52:32 +0800

    feat(hero/section): added hero section with date picker for available tours

---
+++ client/src/components/ui/DataRange.tsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { DateRange } from "react-day-picker";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+export function DatePickerWithRange({
+  className,
+}: React.HTMLAttributes<HTMLDivElement>) {
+  const [date, setDate] = React.useState<DateRange | undefined>(undefined);
+
+  return (
+    <div className={cn("grid gap-2", className)}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            id="date"
+            variant={"outline"}
+            className={cn(
+              "w-[200px] justify-start text-left font-normal text-xs",
+              !date && "text-muted-foreground"
+            )}
+          >
+            <CalendarIcon className="mr-1 h-4 w-4" />
+            {date?.from ? (
+              date.to ? (
+                <>
+                  {format(date.from, "LLL dd, y")} -{" "}
+                  {format(date.to, "LLL dd, y")}
+                </>
+              ) : (
+                format(date.from, "LLL dd, y")
+              )
+            ) : (
+              <span>Select date range</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={date?.from ?? new Date()}
+            selected={date}
+            onSelect={setDate}
+            numberOfMonths={2}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
--- client/src/pages/homepage.tsx
+++ client/src/pages/homepage.tsx
@@ -1,5 +1,8 @@
-import Navbar from "@/components/NavBar"
-
+import Navbar from "@/components/NavBar";
+import { Input } from "@/components/ui/input";
+import { DatePickerWithRange } from "@/components/ui/DataRange";
+import { Button } from "@/components/ui/button";
+import { MapPin } from "lucide-react";
 
 const Homepage = () => {
   return (
@@ -7,10 +10,35 @@ const Homepage = () => {
       <header>
         <Navbar />
       </header>
-      <section>
-        
+      <section className="relative h-[35rem]">
+        <img
+          src="/assets/wallpaper.jpg"
+          alt=""
+          className="h-full w-full object-cover absolute inset-0 z-0"
+        />
+        <div className="absolute inset-0 flex flex-col items-center justify-center z-10">
+          <h1 className="font-bold text-[#fcfcfc] text-2xl">
+            Discover Your Next Destination
+          </h1>
+          <p className=" text-[#fcfcfc] mt-1">
+            Find the perfect getaway-just a few clicks away.
+          </p>
+          <div className="flex bg-[#dedad5] p-2 rounded-sm w-[30rem]">
+            <div className="relative">
+              <Input
+                placeholder="What city can we show you?"
+                className="pl-8 text-sm hover:placeholder:text-black"
+              />
+              <span className="absolute left-2 top-2 text-gray-500">
+                <MapPin className="h-5 w-5 hover:text-black" />
+              </span>
+            </div>
+            <DatePickerWithRange />
+            <Button className="cursor-pointer">Search</Button>
+          </div>
+        </div>
       </section>
     </main>
-  )
-}
-export default Homepage
+  );
+};
+export default Homepage;
