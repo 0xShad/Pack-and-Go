a5db81fea4baa1a96fbb23c70e8c920e3ed59c89
shadrack <castroshadrack7@gmail.com>
2025-04-25 17:37:05 +0800

    refactor(navbar): add currency icons to HoverCard and replace search icon

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,10 +1,20 @@
-import { Search, CircleUserRound } from "lucide-react";
+import { PhilippinePeso, DollarSign, JapaneseYen, IndianRupee,  CircleUserRound } from "lucide-react";
 import { Separator } from "./ui/separator";
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
-import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 
 const Navbar = () => {
+
+ const currency: string[] = ["PHP", "USD", "YEN", "INR"]
+
+ const currencyIcons: Record<string, React.ReactNode> = {
+  PHP: <PhilippinePeso className="w-4 h-4 inline mr-2"/>,
+  USD: <DollarSign className="w-4 h-4 inline mr-2"/>,
+  YEN: <JapaneseYen className="w-4 h-4 inline mr-2" />,
+  INR: <IndianRupee className="w-4 h-4 inline mr-2" />
+ }
+
+
   return (
     <>
       <nav className="w-full flex justify-around p-4 items-center">
@@ -26,11 +36,18 @@ const Navbar = () => {
         <div className="flex gap-10">
           <HoverCard openDelay={200} closeDelay={200}>
             <HoverCardTrigger>
-            <Search className="text-gray-400 hover:text-black cursor-pointer"/>
+           <PhilippinePeso className="text-gray-400 hover:text-black cursor-pointer"/>
             </HoverCardTrigger>
-            <HoverCardContent>
-              <div>
-                <Input type="text" placeholder="Search"/>
+            <HoverCardContent className="w-[6rem]">
+              <div className="flex flex-col gap-5">
+               {
+                currency.map((key) => (
+                  <div key={key} className="flex items-center cursor-pointer">
+                    {currencyIcons[key]}
+                    {key}
+                  </div>
+                ))
+               }
               </div>
             </HoverCardContent>
           </HoverCard>
