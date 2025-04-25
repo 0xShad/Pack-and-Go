3071fa51b61aa3f676526fcb8443b0a06b87874c
shadrack <castroshadrack7@gmail.com>
2025-04-25 17:42:46 +0800

    style(homepage): enhance visual elements by adjusting brightness and typography

--- client/src/pages/homepage.tsx
+++ client/src/pages/homepage.tsx
@@ -14,16 +14,16 @@ const Homepage = () => {
         <img
           src="/assets/wallpaper.jpg"
           alt=""
-          className="h-full w-full object-cover absolute inset-0 z-0"
+          className="h-full w-full object-cover absolute inset-0 z-0 brightness-50"
         />
         <div className="absolute inset-0 flex flex-col items-center justify-center z-10">
-          <h1 className="font-bold text-[#fcfcfc] text-2xl">
+          <h1 className="font-bold text-[#fcfcfc] text-3xl">
             Discover Your Next Destination
           </h1>
-          <p className=" text-[#fcfcfc] mt-1">
+          <p className=" text-gray-200 mt-2 text-lg font-light">
             Find the perfect getaway-just a few clicks away.
           </p>
-          <div className="flex bg-[#dedad5] p-2 rounded-sm w-[30rem]">
+          <div className="flex bg-[#dedad5] p-2 rounded-sm w-[30rem] mt-5">
             <div className="relative">
               <Input
                 placeholder="What city can we show you?"
