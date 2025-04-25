8a460e4b6463244da20c0bde289c794b5f8bd3c7
shadrack <castroshadrack7@gmail.com>
2025-04-25 21:29:46 +0800

    refactor(navbar): improve navbar structure and enhance currency selection functionality

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,19 +1,28 @@
-import { PhilippinePeso, DollarSign, JapaneseYen, IndianRupee,  CircleUserRound } from "lucide-react";
+import {
+  PhilippinePeso,
+  DollarSign,
+  JapaneseYen,
+  IndianRupee,
+  CircleUserRound,
+} from "lucide-react";
 import { Separator } from "./ui/separator";
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
 import { Button } from "./ui/button";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [navState, setNavState] = useState("Home");
+  const [preparedCurrency, setPreparedCurrency] = useState("PHP");
+  const currency: string[] = ["PHP", "USD", "YEN", "INR"];
 
- const currency: string[] = ["PHP", "USD", "YEN", "INR"]
+  const currencyIcons: Record<string, React.ReactNode> = {
+    PHP: <PhilippinePeso className="w-7 h-5 inline mr-2" />,
+    USD: <DollarSign className="w-4 h-4 inline mr-2" />,
+    YEN: <JapaneseYen className="w-4 h-4 inline mr-2" />,
+    INR: <IndianRupee className="w-4 h-4 inline mr-2" />,
+  };
 
- const currencyIcons: Record<string, React.ReactNode> = {
-  PHP: <PhilippinePeso className="w-4 h-4 inline mr-2"/>,
-  USD: <DollarSign className="w-4 h-4 inline mr-2"/>,
-  YEN: <JapaneseYen className="w-4 h-4 inline mr-2" />,
-  INR: <IndianRupee className="w-4 h-4 inline mr-2" />
- }
-
+  const navLinks = ["Home", "Tours", "About", "Contact"];
 
   return (
     <>
@@ -26,45 +35,57 @@ const Navbar = () => {
           />
         </div>
         <div className="hidden md:block">
-          <ul className="flex gap-10">
-            <li className="text-gray-400 text-md hover:text-black cursor-pointer">Home</li>
-            <li className="text-gray-400 text-md hover:text-black cursor-pointer">About</li>
-            <li className="text-gray-400 text-md hover:text-black cursor-pointer">Services</li>
-            <li className="text-gray-400 text-md hover:text-black cursor-pointer">Contact</li>
-          </ul>
+          <div className="flex gap-10">
+            {navLinks.map((link) => (
+              <button
+                className={`${
+                  link === navState ? "text-black" : "text-gray-400"
+                } cursor-pointer`}
+                onClick={() => setNavState(link)}
+              >
+                {link}
+              </button>
+            ))}
+          </div>
         </div>
         <div className="flex gap-10">
           <HoverCard openDelay={200} closeDelay={200}>
             <HoverCardTrigger>
-           <PhilippinePeso className="text-gray-400 hover:text-black cursor-pointer"/>
+              <div className="text-gray-400 hover:text-black cursor-pointer">
+                {currencyIcons[preparedCurrency]}
+              </div>
             </HoverCardTrigger>
             <HoverCardContent className="w-[6rem]">
               <div className="flex flex-col gap-5">
-               {
-                currency.map((key) => (
-                  <div key={key} className="flex items-center cursor-pointer">
+                {currency.map((key) => (
+                  <div
+                    key={key}
+                    className="flex items-center cursor-pointer"
+                    onClick={() => setPreparedCurrency(key)}
+                  >
                     {currencyIcons[key]}
                     {key}
                   </div>
-                ))
-               }
+                ))}
               </div>
             </HoverCardContent>
           </HoverCard>
           <HoverCard openDelay={200} closeDelay={200}>
             <HoverCardTrigger>
-            <CircleUserRound className="text-gray-400 hover:text-black cursor-pointer"/>
+              <CircleUserRound className="text-gray-400 hover:text-black cursor-pointer" />
             </HoverCardTrigger>
             <HoverCardContent>
               <div className="flex gap-3 flex-col p-2">
                 <Button className="cursor-pointer">Log in</Button>
-                <Button className="cursor-pointer bg-white text-black border-2 hover:bg-black hover:text-white">Sign up</Button>
+                <Button className="cursor-pointer bg-white text-black border-2 hover:bg-black hover:text-white">
+                  Sign up
+                </Button>
               </div>
             </HoverCardContent>
           </HoverCard>
         </div>
       </nav>
-      <Separator/>
+      <Separator />
     </>
   );
 };
