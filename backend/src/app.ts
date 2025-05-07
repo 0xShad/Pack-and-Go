b343652e36b9d8ca6057e60ae151c76cc444358d
shadrack <castroshadrack7@gmail.com>
2025-05-07 16:19:56 +0800

    refactor(app): update CORS middleware and JSON parsing settings

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,13 +12,9 @@ const app = express();
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
-app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json({ limit: "50mb" }));
+console.log("CORS middleware running");
 
 // Routes
 app.use("/auth", authRouter);
@@ -32,6 +28,6 @@ app.get("/", (req: express.Request, res: express.Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on PORT: 8080`);
+  console.log(`Server is running on PORT: ${PORT}`);
   connectToDatabase();
 });
